37f435311a6096c06bdf1ea022e6a3a6db2009cb
ing-arriola <ingeniero.arriola@gmail.com>
2020-12-14 06:56:27 -0600

    Added text input and buttons

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 import Header from "./Components/Header";
 import StartGame from "./Screens/StartGame";
@@ -16,5 +16,7 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+  },
 });
--- Screens/StartGame.js
+++ Screens/StartGame.js
@@ -1,9 +1,18 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 const StartGame = () => {
   return (
     <View style={styles.screen}>
-      <Text>Game Screen</Text>
+      <Text style={styles.title}>Start new game!!</Text>
+      <View style={styles.inputContainer}>
+        <Text>Enter a new number</Text>
+        <TextInput />
+
+        <View style={styles.buttonContainer}>
+          <Button title="Confirm" />
+          <Button title="Reset" />
+        </View>
+      </View>
     </View>
   );
 };
@@ -13,6 +22,21 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
     alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  inputContainer: {
+    alignItems: "center",
+    width: 300,
+    maxWidth: "80%",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    paddingHorizontal: 10,
   },
 });
 
