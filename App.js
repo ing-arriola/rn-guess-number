24a1de035160b7759132a5bde8fd8b937ec98bc7
ing-arriola <ingeniero.arriola@gmail.com>
2021-05-03 18:13:34 -0600

    Added logic to set a win status

--- App.js
+++ App.js
@@ -1,30 +1,30 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 import Header from "./Components/Header";
 import StartGame from "./Screens/StartGame";
 import GameScreen from "./Screens/GameScreen";
+import GameOverScreen from './Screens/GameOverScreen'
 
 export default function App() {
   const [userNUmber, setUserNUmber] = useState();
+  const [win,setWin] = useState(false)
 
   const startGameHandler = (selectedNumber) => {
-    console.log("holassss");
     setUserNUmber(selectedNumber);
   };
-  console.log(userNUmber);
   let content = <StartGame startGameHandler={startGameHandler} />;
 
   if (userNUmber) {
-    content = <GameScreen userChoice={userNUmber} />;
+    content = <GameScreen userChoice={userNUmber} setWin={setWin}  />;
   }
 
   return (
     <View style={styles.container}>
       <Header title="Guess the number" />
       <StatusBar style="auto" />
-      {content}
+      {win ? <GameOverScreen setUserNUmber={setUserNUmber} setWin={setWin} />  : content}
     </View>
   );
 }
