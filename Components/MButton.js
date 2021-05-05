d21c62de2f4d11ed95c332d0f6a8dad58933245c
ing-arriola <ingeniero.arriola@gmail.com>
2021-05-05 09:19:44 -0600

    Button is now able to display diffent variants

--- Components/MButton.js
+++ Components/MButton.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import {View,Text,StyleSheet,TouchableOpacity} from 'react-native'
+import {View,Text,StyleSheet,TouchableOpacity, Button} from 'react-native'
 import Colors from '../Constants/Colors'
 
 const MButton = (props) => {
     return (
         <TouchableOpacity onPress={props.onPress} >
-            <View style={styles[props.variant]} >
+            <View style={{...styles.button,...styles[props.variant]}} >
                 <Text style={styles.textButton} >
                     {props.children}
                 </Text>
@@ -21,18 +21,20 @@ const styles = StyleSheet.create({
         fontSize:15,
         textAlign:'center'
     },
-    primary:{
-        backgroundColor:Colors.primary,
+    button:{
         paddingHorizontal:20,
         paddingVertical:12,
         borderRadius:5  
     },
+    primary:{
+        backgroundColor:Colors.primary,
+    },
     alert:{
         backgroundColor:Colors.alert,
-        paddingHorizontal:20,
-        paddingVertical:12,
-        borderRadius:5  
-    }
+    },
+    success600:{
+        backgroundColor:Colors.success600,
+    },
 })
 
 export default MButton
