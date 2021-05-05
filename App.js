32cf6cffa12d59dbc53347f16548d6442e17f9c9
ing-arriola <ingeniero.arriola@gmail.com>
2021-05-05 02:46:37 -0600

    Added logic to load custom fonts

--- App.js
+++ App.js
@@ -1,15 +1,32 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
+import * as Font from 'expo-font'
 import { StyleSheet, View } from "react-native";
+import AppLoading from 'expo-app-loading'
 
 import Header from "./Components/Header";
 import StartGame from "./Screens/StartGame";
 import GameScreen from "./Screens/GameScreen";
 import GameOverScreen from './Screens/GameOverScreen'
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans':require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold':require('./assets/fonts/OpenSans-Bold.ttf')
+  })
+}
+
 export default function App() {
   const [userNUmber, setUserNUmber] = useState();
   const [win,setWin] = useState(false)
+  const [loaded,setLoaded] = useState(false)
+
+  if(!loaded){
+    return <AppLoading 
+              startAsync={fetchFonts} 
+              onFinish={()=>setLoaded(true)} 
+              onError={(err) => console.log(err)}/>
+  }
 
   const startGameHandler = (selectedNumber) => {
     setUserNUmber(selectedNumber);
